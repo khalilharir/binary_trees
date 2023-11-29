47036028d9066bc30b44839aade308374035fb3a
khalilharir <khalil.harir2@gmail.com>
2023-11-29 08:43:01 -0800

    commit preorder

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -7,7 +7,7 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (func && tree)
+	if (tree && func)
 	{
 		func(tree->n);
 		binary_tree_preorder(tree->right, func);
